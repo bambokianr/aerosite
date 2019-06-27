f7e659feae57203ebe1c2baf2bd913d58461dbd1
crestanigabriel <crestanigabriel@gmail.com>
2019-06-26 22:24:54 -0300

    integracao front e back

--- pages/Contato/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  background-color: #999999;
-  color: rgb(255, 255, 255);
-`;
---
+++ workhere/aerosite-back/static/react/precache-manifest.bd16bb4e1aeb220bc892f5a151614b6b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92b10fc2610c3ef64216ece6df4acca",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "ce4901ccee01f8da3e70",
+    "url": "/static/react/static/css/2.0e2d9fca.chunk.css"
+  },
+  {
+    "revision": "ce4901ccee01f8da3e70",
+    "url": "/static/react/static/js/2.4d0c8db3.chunk.js"
+  },
+  {
+    "revision": "77099c9a497f2b840a7e",
+    "url": "/static/react/static/js/main.5930638e.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/static/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "e4dad19c00d1280564e32efa95d1b9b5",
+    "url": "/static/react/static/media/apoiopatrocinio.e4dad19c.png"
+  },
+  {
+    "revision": "179ac7b024c48a292cd1014bc10c3ddc",
+    "url": "/static/react/static/media/area_membros.179ac7b0.png"
+  },
+  {
+    "revision": "5a78d164f9f54e5af014d2f16863d7bc",
+    "url": "/static/react/static/media/areamembros.5a78d164.png"
+  },
+  {
+    "revision": "edeeda57ff64cf20b5024a7e0d1bdaf9",
+    "url": "/static/react/static/media/box_advanced.edeeda57.png"
+  },
+  {
+    "revision": "f0b3b26d6fc1d8346b3087d1fb25d0e6",
+    "url": "/static/react/static/media/box_micro.f0b3b26d.png"
+  },
+  {
+    "revision": "af55099040954ceb86b78a246b8b4651",
+    "url": "/static/react/static/media/box_regular.af550990.png"
+  },
+  {
+    "revision": "9f8fff005074f156c199bbd388f9ec98",
+    "url": "/static/react/static/media/contato.9f8fff00.png"
+  },
+  {
+    "revision": "a6ce4a31c19aa75ff5ea40463c3ffc4f",
+    "url": "/static/react/static/media/depoimentos.a6ce4a31.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fdc0f5401092b77154148e5e055ef3f2",
+    "url": "/static/react/static/media/icondepo.fdc0f540.png"
+  },
+  {
+    "revision": "19402296ba9448e57f5166aa8c97d993",
+    "url": "/static/react/static/media/intro.19402296.png"
+  },
+  {
+    "revision": "ea2b5364d1be006f85a00ca2d08f1ac7",
+    "url": "/static/react/static/media/intro2.ea2b5364.png"
+  },
+  {
+    "revision": "439daaa9d95889737c4609f2f0554cb8",
+    "url": "/static/react/static/media/logoaviao.439daaa9.png"
+  },
+  {
+    "revision": "5700cd54a6dc2e2202ff6ebcec8d7cad",
+    "url": "/static/react/static/media/logobranco.5700cd54.png"
+  },
+  {
+    "revision": "5617b120ad4d4e438a3093f36d12a138",
+    "url": "/static/react/static/media/news1.5617b120.png"
+  },
+  {
+    "revision": "e7aa4cddd10a6e523ed3da37dc41ad23",
+    "url": "/static/react/static/media/noticias.e7aa4cdd.png"
+  },
+  {
+    "revision": "9d7027a1f860b80ef87be5a5502aa788",
+    "url": "/static/react/static/media/perfil_membro.9d7027a1.png"
+  },
+  {
+    "revision": "e3fbb64246a69688b7891ed1a2812e3e",
+    "url": "/static/react/static/media/planomkt_aerodesignITA.e3fbb642.pdf"
+  },
+  {
+    "revision": "0c742fd46759edb344d31012d6233038",
+    "url": "/static/react/static/media/qsomos.0c742fd4.png"
+  }
+]);
